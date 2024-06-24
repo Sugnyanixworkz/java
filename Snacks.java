09d32cdf0b606038e35ebc4175b5caced2336f28
Dr.nagaraj Gandigudi <sugnyani.xworkz@gmail.com>
2024-06-24 13:08:47 +0530

    This is my first commit

---
+++ Snacks.java
@@ -0,0 +1,43 @@
+public class Snacks
+{
+public static void main  (String args[])
+{
+
+System.out.println("Local variables:Snacks");
+
+String pakoda   ="Pakoda";
+System.out.println(""+pakoda);
+
+String   vadapav    ="";
+System.out.println(""+vadapav);
+
+String  potatochip     ="Potato chip";
+System.out.println(""+potatochip);
+
+String  dabeli     ="Dabeli";
+System.out.println(""+dabeli);
+
+String   jerky  ="Jerkey";
+System.out.println(""+jerky);
+
+String khakra      ="Khakra";
+System.out.println(""+khakra);
+
+String   cheese    ="Cheese";
+System.out.println(""+cheese);
+
+
+
+String  popcorn     ="Popcorn";
+System.out.println(""+popcorn );
+
+
+String  samosa    ="Samosa";
+System.out.println(""+samosa );
+
+
+
+
+
+}
+}
