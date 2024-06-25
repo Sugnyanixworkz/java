b14ed536f8fc653bc5cb12707c912c5309f3fd3e
Dr.nagaraj Gandigudi <sugnyani.xworkz@gmail.com>
2024-06-25 09:45:35 +0530

    This is my first commit

---
+++ Bus.java
@@ -0,0 +1,32 @@
+ public class Bus
+{
+public static void main(String args[])
+{
+System.out.println("Local variables:");
+
+String busName="Amoghavarsha";
+int ticketPrize=800;
+String departure="Hydrabad";
+String arrival="Bhalki";
+String busType="AC";
+String driverName="Ayush";
+String conductorName="Vyas";
+boolean statusOfTicket=true;
+char gender='F';
+
+if( ticketPrize<100 )
+{
+
+System.out.println("The ticket prize is"+ticketPrize+" and it will travell from "+departure+" to "+arrival);
+}
+else
+{
+System.out.println("The condition is false");
+}
+
+
+
+
+
+}
+}
