792265f154ace3f10cf2661ad0ab8ae417fc7886
Dr.nagaraj Gandigudi <sugnyani.xworkz@gmail.com>
2024-06-25 09:50:16 +0530

    commit message

---
+++ Charger.java
@@ -0,0 +1,16 @@
+public class Charger
+{
+	public static String name="Lenovo";
+	public static int prize=1000;
+	public static byte pins=3;
+	public static short cables=1;
+	public static float version=8.1f;
+	public static char type='o';
+	public static boolean active=true;
+	public static double phnVersion=8;
+	
+	public static String color="black";
+	public static char gender='F';
+	
+	
+}
