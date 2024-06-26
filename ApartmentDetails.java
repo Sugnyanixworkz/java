9b0609eea2a4dee003393e870643e23ad75dd63b
Dr.nagaraj Gandigudi <sugnyani.xworkz@gmail.com>
2024-06-26 14:08:09 +0530

    25 jun

---
+++ ApartmentDetails.java
@@ -0,0 +1,4 @@
+public class ApartmentDetails
+{
+public static int apartmentFlores[]={1,2,3,4};
+}
---
+++ ApartmentRunner.java
@@ -0,0 +1,18 @@
+public class ApartmentRunner
+{
+public static void main(Strin args[])
+{
+System.out.println(" Intro about Apartment");
+
+for(int index=0 ;index< Apartment.apartmentNames.length;index++)
+{
+System.out.println("Apartment names is"+ apartmentNames[index]);
+}
+
+for(int indexx=0;indexx<ApartmentDetails.apartmentFlores.length;indexx++)
+{
+System.out.println("Apartment flores are " +ApartmentDetails.apartmentFlores[indexx]);
+}
+
+}
+}
