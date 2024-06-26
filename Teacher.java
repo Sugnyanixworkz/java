cf04e9ae7bcc90f4e490b4615aa1e5ce62298428
Dr.nagaraj Gandigudi <sugnyani.xworkz@gmail.com>
2024-06-26 14:40:15 +0530

    25 jun

---
+++ Teacher.java
@@ -0,0 +1,39 @@
+public class Teacher
+{
+public static void main(String args[])
+{
+String name[]={"Tika","liyas","Shyarab","Twinkle"};
+int age[]={25,30,40,50,60};
+long phn[]={26313512624l,2724627666l,1276183222l,671167276l};
+char gen[]={'M','F','F','M'};
+
+for(int index=0;index<name.length;index++)
+{
+System.out.println("The name of the teacher is "+name[index]);
+}
+
+for(int index=0;index<age.length;index++)
+{
+System.out.println("The age of the teacher is "+age[index]);
+}
+
+for(int index=0;index<phn.length;index++)
+{
+System.out.println("The phnNo. of the teacher is "+phn[index]);
+}
+
+for(int index=0;index<gen.length;index++)
+{
+System.out.println("The gender of the teacher is "+gen[index]);
+
+}
+}
+
+
+
+}
+   
+
+
+
+
