1ffd4cdbfdc86e112aff369e0ca6cf57f2d6dd5e
Dr.nagaraj Gandigudi <sugnyani.xworkz@gmail.com>
2024-06-27 21:34:08 +0530

    26 jun

---
+++ Trainee.java
@@ -0,0 +1,13 @@
+public class Trainee
+{
+
+public static void main(String args[])
+{
+
+for(int index=0;index<args.length;index++)
+{
+System.out.println("The ELEMENT  " +args[index]);
+}
+System.out.println("The LENGTH " +args.length);
+}
+}
